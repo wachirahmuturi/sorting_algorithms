2e444889170e00247247e461a9b598ed6806bfef
wachirahmuturi <wachiramuturi18@gmail.com>
2023-02-18 12:47:57 +0000

    Sorting algorithms added

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -29,4 +30,9 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
